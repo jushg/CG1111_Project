bd597d0d6bd9fc4e18c926570b4b436044821e0e
hozhenhong99 <69459292+hozhenhong99@users.noreply.github.com>
2020-10-28 16:53:32 +0800

    Update motors.ino

--- Driver_Code/motors/motors.ino
+++ Driver_Code/motors/motors.ino
@@ -1,15 +1,25 @@
 #include <MeMCore.h>
 
-int i = 100;
+int i = 150;
+//int a = 0;
+//int b = 0;
 MeDCMotor motor1(M1); //left motor
 MeDCMotor motor2(M2); //right motor
 
 void setup() 
 {
   turnLeft();
-  //turnRight();
+  delay(2000);
+  turnRight();
+  delay(2000);
   //moveForward();
+  UTurn();
+}
 
+void moveForward()
+{
+  motor1.run(-i);
+  motor2.run(i);
 }
 
 void stop()
@@ -22,27 +32,64 @@ void turnLeft()
 {
   motor1.run(i);
   motor2.run(i);
-  delay(1000);
-  motor1.stop();
-  motor2.stop();
+  delay(470);
+  stop();
 }
 
 void turnRight()
 {
   motor1.run(-i);
   motor2.run(-i);
-  delay(500);
-  motor1.stop();
-  motor2.stop();
+  delay(470);
+  stop();
 }
 
-void moveForward()
+void UTurn()
 {
-  motor1.run(-i);
-  motor2.run(i);
+  turnLeft();
+  turnLeft();
 }
 
-void loop() 
+void loop()
 {
-
+  
 }
+
+/*
+void doubleLeft()
+{
+  turnLeft();
+  while(data based on ultrasonic sensor)
+  {
+    moveForward();
+  }
+  turnLeft();
+}
+
+void doubleRight()
+{
+  turnLeft();
+  while(data based on ultrasonic sensor)
+  {
+    moveForward();
+  }
+  turnLeft();
+}
+
+void adjustLeft() // to be used if mbot is not moving straight based on data from the ir sensors
+{
+ while()
+ {
+   motor1.run(-i);
+   motor2.run(i+10);
+ }
+}
+
+void adjustRight() // to be used if mbot is not moving straight based on data from the ir sensors
+{
+ while()
+ {
+   motor1.run(-i-10);
+   motor2.run(i);
+ }
+}*/
