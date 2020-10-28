b9158765d0fe8c64b3d5475089dcb1146f13e9ae
trongtan282 <tanht282@gmail.com>
2020-10-28 14:22:12 +0800

    Update ultrasonic_sensor.ino

--- Driver_Code/ultrasonic_sensor/ultrasonic_sensor.ino
+++ Driver_Code/ultrasonic_sensor/ultrasonic_sensor.ino
@@ -1,35 +1,19 @@
-#define TRIGGER 12
-#define ECHO 11
-#define TIMEOUT 30000
-#define WAITING_TIME 1000
 
-long duration; 
-double distance;
-//The following code is run once at the start of execution to setup the different peripherals
 
-void setup() 
-{ 
-  pinMode(TRIGGER, OUTPUT); 
-  digitalWrite(TRIGGER, LOW); 
-  pinMode(ECHO, INPUT); 
-  
-  // Set up the serial Communication
-  //Serial.begin(9600); 
+
+#include "MeOrion.h"
+
+MeUltrasonicSensor ultraSensor(PORT_7); /* Ultrasonic module can ONLY be connected to port 3, 4, 6, 7, 8 of base shield. */
+
+void setup()
+{
+  Serial.begin(9600);
 }
-void loop() 
-{ 
-  //The following code is run repeatedly 
-  digitalWrite(TRIGGER, HIGH); 
-  delayMicroseconds(10); 
-  digitalWrite(TRIGGER, LOW); 
-  delayMicroseconds(10); 
-  
-  duration = pulseIn(ECHO, HIGH, TIMEOUT); 
-  // Calculate the distance base on the duration
-  distance = (duration/2.0);
-  //Serial.print("Duration: ");
-  //Serial.print(duration); 
-  //Serial.println(" microseconds"); 
-  
-  delay(WAITING_TIME); 
+
+void loop()
+{
+  Serial.print("Distance : ");
+  Serial.print(ultraSensor.distanceCm() );
+  Serial.println(" cm");
+  delay(100); /* the minimal measure interval is 100 milliseconds */
 }
